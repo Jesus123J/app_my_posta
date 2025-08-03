e379da736c76783ff179980970c70ba2ae9cb362
Thiago <gutierrezdelatorrej30@gmail.com>
2025-08-02 21:25:09 -0500

    first commitment

---
+++ src/main/java/com/mi_posta/microservicio_backend/MicroserviceBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mi_posta.microservicio_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mi_posta/microservicio_backend/MicroserviceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mi_posta.microservicio_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
